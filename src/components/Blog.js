7d382708686d3522225cff1f81e71713a801a148
Tanvir_Hossen <tanvirhossen853@gmail.com>
2024-01-02 12:14:36 +0600

    Resturant app react

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { blog } from '../data'
 
 const Blog = () => {
   return (
@@ -6,6 +7,25 @@ const Blog = () => {
         <h1 className='heading'>
             <span>OUR </span> BLOGS
         </h1>
+        <div className='box-container'>
+            {
+                blog.map((item,index)=>(
+                    <div className='box' key={index}>
+                        <div className='image'>
+                            <img src={item.img} alt='#nothing'/>
+                        </div>
+                        <div className='content'>
+                            <a href='#nothing' className='title'>
+                                Tasty and refreshing spices
+                            </a>
+                            <span>by admin / 21st May, 2023</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum, modi?</p>
+                            <a href='#nothing' className='btn'>READ MORE</a>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
     </section>
   )
 }
